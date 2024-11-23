f85b0f5c3ece3dccbe4569e02fa1787bfb2dc327
A-Amiin <ahmed.m.aminn@gmail.com>
2024-11-23 16:16:23 +0200

    adds the routes links and test it with post man

--- server-side/server.js
+++ server-side/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 
 const app = express();
+
+app.use("/api/users", require('./routes/users'));
+app.use("/api/profile", require('./routes/profile'));
+app.use("/api/auth", require('./routes/auther'));
+app.use("/api/posts", require('./routes/posts'));
+
+// Connect to the database
+connection();
 app.get('/', (req, res) => res.send("Server working Correct"));
 
 const PORT = process.env.PORT || 4000;
